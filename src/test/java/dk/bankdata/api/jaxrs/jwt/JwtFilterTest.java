d82090af2d4780cd1d67ee901354a898d584f2f5
Kenneth Bøgedal <bdukbh@bankdata.dk>
2019-02-05 09:52:39 +0100

    :hammer: Fixed checkstyle

--- src/test/java/dk/bankdata/api/jaxrs/jwt/JwtFilterTest.java
+++ src/test/java/dk/bankdata/api/jaxrs/jwt/JwtFilterTest.java
@@ -6,6 +6,10 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Response;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,17 +19,13 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ResourceInfo;
-import javax.ws.rs.core.Response;
-
 @RunWith(MockitoJUnitRunner.class)
 public class JwtFilterTest {
     @InjectMocks @Spy JwtFilter jwtFilter;
     @Mock ResourceInfo resourceInfo;
 
     @Test
-    public void shouldEscapeIFAnnotatedWithOpenApi() throws Exception {
+    public void shouldEscapeIfAnnotatedWithOpenApi() throws Exception {
         when(resourceInfo.getResourceMethod()).thenReturn(TestMethods.class.getDeclaredMethod("methodWithAnnotation"));
 
         jwtFilter.filter(null);
