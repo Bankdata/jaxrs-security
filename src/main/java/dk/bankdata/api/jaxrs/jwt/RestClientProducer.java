7294eed46cef3c5a837a8c09d523901d188cdfb6
Kenneth Bøgedal <kbh@bankdata.dk>
2019-02-06 19:18:52 +0100

    :hammer:Refactored the usage of environment out of RestClientProducer and replaced it with annotation

--- src/main/java/dk/bankdata/api/jaxrs/jwt/RestClientProducer.java
+++ src/main/java/dk/bankdata/api/jaxrs/jwt/RestClientProducer.java
@@ -10,8 +10,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
-import dk.bankdata.api.jaxrs.environment.Environment;
-
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 import java.net.URI;
@@ -20,7 +20,8 @@ import java.util.concurrent.TimeUnit;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.util.Nonbinding;
 import javax.inject.Qualifier;
 import javax.ws.rs.client.Client;
 
@@ -28,25 +29,10 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 
 @ApplicationScoped
 public class RestClientProducer {
-    @Inject Environment environment;
+    private String proxyUrl;
 
     private static final long CONNECTION_TIMEOUT = 10000;
     private static final long READ_TIMEOUT = 10000;
-
-    @Produces
-    @RequestScoped
-    @WithProxy
-    public Client produceClient() {
-        ObjectMapper objectMapper = createObjectMapper();
-        URI proxy = URI.create(environment.getProxyUrl());
-        ResteasyClientBuilder builder = new ResteasyClientBuilder()
-                .establishConnectionTimeout(CONNECTION_TIMEOUT, TimeUnit.MILLISECONDS)
-                .socketTimeout(READ_TIMEOUT, TimeUnit.MILLISECONDS)
-                .register(new JacksonJsonProvider(objectMapper))
-                .defaultProxy(proxy.getHost(), proxy.getPort(), proxy.getScheme());
-
-        return builder.build();
-    }
 
     @Produces
     @RequestScoped
@@ -61,16 +47,47 @@ public class RestClientProducer {
         return builder.build();
     }
 
+    @Produces
+    @RequestScoped
+    @WithProxy
+    public Client produceClient(InjectionPoint injectionPoint) {
+        this.proxyUrl = initProxy(injectionPoint);
+
+        ObjectMapper objectMapper = createObjectMapper();
+        URI proxy = URI.create(proxyUrl);
+        ResteasyClientBuilder builder = new ResteasyClientBuilder()
+                .establishConnectionTimeout(CONNECTION_TIMEOUT, TimeUnit.MILLISECONDS)
+                .socketTimeout(READ_TIMEOUT, TimeUnit.MILLISECONDS)
+                .register(new JacksonJsonProvider(objectMapper))
+                .defaultProxy(proxy.getHost(), proxy.getPort(), proxy.getScheme());
+
+        return builder.build();
+    }
+
     private ObjectMapper createObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
         return objectMapper;
     }
 
+    private String initProxy(InjectionPoint injectionPoint) {
+        for (Annotation annotation : injectionPoint.getQualifiers()) {
+            if (annotation.annotationType().equals(WithProxy.class)) {
+                return ((WithProxy) annotation).value();
+            }
+        }
+
+        throw new IllegalStateException("No @WithProxy on InjectionPoint");
+    }
+
     @Qualifier
     @Retention(RUNTIME)
     @Target({TYPE, METHOD, FIELD, PARAMETER})
-    public @interface WithProxy {}
+    @Documented
+    public @interface WithProxy {
+        @Nonbinding
+        String value() default "";
+    }
 
     @Qualifier
     @Retention(RUNTIME)
