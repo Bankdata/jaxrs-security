a256c665fdb65ceda67dabc6811be792d5ebc783
Kenneth Bøgedal <kbh@bankdata.dk>
2019-02-08 20:46:28 +0100

    :white_check_mark:Added unit-test for CorsFilter

--- src/test/java/dk/bankdata/api/jaxrs/cors/CorsFilterTest.java
+++ src/test/java/dk/bankdata/api/jaxrs/cors/CorsFilterTest.java
@@ -11,15 +11,13 @@ import javax.ws.rs.core.MultivaluedMap;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CorsFilterTest {
-    @InjectMocks CorsFilter corsFilter;
 
     @Test
-    public void shouldAddHeaders() {
+    public void shouldAddHeadersDefaultHeaders() {
         ContainerRequestContext requestContext = mock(ContainerRequestContext.class);
 
         MultivaluedMap<String, Object> headers = new MultivaluedHashMap<>();
@@ -28,6 +26,7 @@ public class CorsFilterTest {
         when(requestContext.getHeaderString("origin")).thenReturn("some-origin");
         when(responseContext.getHeaders()).thenReturn(headers);
 
+        CorsFilter corsFilter = new CorsFilter();
         corsFilter.filter(requestContext,responseContext);
 
         MultivaluedMap<String, Object> result = responseContext.getHeaders();
@@ -37,6 +36,33 @@ public class CorsFilterTest {
                 result.get("Access-Control-Allow-Headers").toString());
         Assert.assertEquals("[some-origin]", result.get("Access-Control-Allow-Origin").toString());
         Assert.assertEquals("[1728000]", result.get("Access-Control-Max-Age").toString());
+        Assert.assertEquals("[Origin]", result.get("Vary").toString());
+    }
+
+    @Test
+    public void shouldAddHeadersCustomHeaders() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration()
+                .allowHeaders("some-headers")
+                .allowMethods("some-methods")
+                .maxAge("12345");
+
+        ContainerRequestContext requestContext = mock(ContainerRequestContext.class);
+
+        MultivaluedMap<String, Object> headers = new MultivaluedHashMap<>();
+        ContainerResponseContext responseContext = mock(ContainerResponseContext.class);
+
+        when(requestContext.getHeaderString("origin")).thenReturn("some-origin");
+        when(responseContext.getHeaders()).thenReturn(headers);
+
+        CorsFilter corsFilter = new CorsFilter(corsConfiguration);
+        corsFilter.filter(requestContext,responseContext);
+
+        MultivaluedMap<String, Object> result = responseContext.getHeaders();
+
+        Assert.assertEquals("[12345]", result.get("Access-Control-Max-Age").toString());
+        Assert.assertEquals("[some-methods]", result.get("Access-Control-Allow-Methods").toString());
+        Assert.assertEquals("[some-headers]", result.get("Access-Control-Allow-Headers").toString());
+        Assert.assertEquals("[some-origin]", result.get("Access-Control-Allow-Origin").toString());
         Assert.assertEquals("[Origin]", result.get("Vary").toString());
     }
 }
